9c6238c25444847603f90c90a1075a1aeb23b67f
Shabbadoo26 <38171710+Shabbadoo26@users.noreply.github.com>
2019-09-24 19:13:50 -0400

    added form control

--- character_names/app/routes.py
+++ character_names/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 from . import nameGen
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -11,3 +12,8 @@ def index():
 def genPage():
     names = nameGen.arrGen(20)
     return render_template("generator.html", title="Generator", names=names)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
